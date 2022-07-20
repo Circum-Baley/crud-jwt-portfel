6dc75ec36060a35000878425a516ab688f70ed8c
Circum-Baley <gonzalo.bahamondez@gmail.com>
2022-07-20 01:54:19 -0400

    Initialization

---
+++ crud-jwt-portfel/src/main/java/com/cirbal/portfel/CrudJwtPortfelApplication.java
@@ -0,0 +1,13 @@
+package com.cirbal.portfel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJwtPortfelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJwtPortfelApplication.class, args);
+	}
+
+}
---
+++ crud-jwt-portfel/src/test/java/com/cirbal/portfel/CrudJwtPortfelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirbal.portfel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJwtPortfelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
